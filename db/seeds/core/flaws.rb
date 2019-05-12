e3f6ed94ea8439560f2dc941755183009cc0ba4d
mesona <kylemesona@gmail.com>
2019-05-11 18:29:34 -0700

    Add more flaws

--- db/seeds/core/flaws.rb
+++ db/seeds/core/flaws.rb
@@ -190,6 +190,260 @@ module Flaws
     book: "Core",
     major: true)
 
+  deaf = Flaw.create!(name: "Deaf"),
+    descrtiption: "You cannot hear at all. You may be able to speak, but you cannot understand spoken lan- guage, which makes communication difficult. Magi with this flaw must roll an extra two botch dice when casting spells with a spoken compo- nent, as they cannot hear what they are saying.",
+    flaw_type: "General",
+    book: "Core",
+    major: true)
+
+  deficient_form = Flaw.create!(name: "Deficient Form"),
+    descrtiption: "Almost all totals (including Casting Totals and Lab Totals, but excluding Magic Resistance) to which a particular Form is added are halved. The Form has its full value for the purposes of studying it, and study totals are not halved.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: false)
+
+  deficient_technique = Flaw.create!(name: "Deficient Technique"),
+    descrtiption: "All totals, including Lab and Casting totals, including a particular Technique are halved. Study totals are not halved. Experience points required are based on the actual value of the Technique, before halving.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: true)
+
+  deleterious_circumstances = Flaw.create!(name: "Deleterious Circumstances"),
+    descrtiption: "All your magic totals are halved under certain uncommon circumstances. This can be your state, such as sitting or wet, the target of the magic, such as wild animals or iron, or the place where you are casting the magic, such as a city or high up a mountain.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: false)
+
+  delusion = Flaw.create!(name: "Delusion"),
+    descrtiption: "You believe wholeheartedly in something that just isn’t so. Examples include that you are a magus (if you aren’t), that you are the Pope’s child, or that your imaginary friend is real. This can cause real problems for you and your associates.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  dependent = Flaw.create!(name: "Dependent"),
+    descrtiption: "You feel responsible for someone, and bound to help if he is in trouble. This person may be a rel- ative or friend, but should be relatively weak and a non-player character. If your Dependent passes beyond your ability to help, or becomes too pow- erful, you should substitute another Story Flaw. Possibilities include taking the children of the old Dependent as new Dependents, taking the killers of the Dependent as Enemies, or taking the Dependent as a True Friend.",
+    flaw_type: "Story",
+    book: "Core",
+    major: true)
+
+  depressed = Flaw.create!(name: "Depressed"),
+    descrtiption: "You have a dark outlook on life, seeing lit- tle point in acting and taking no pleasure in anything that happens.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  diabolic_past = Flaw.create!(name: "Diabolic Past"),
+    descrtiption: "You were associated with diabolists, and though you have escaped their evil ways, you are still haunted by your upbringing and the memory of acts best left unspoken. Perhaps your parents were diabolists, although you were not, or maybe you joined a cult but soon repented and fled. Your former associates still take an interest in your activities and where- abouts. Unfortunately. You may purchase the Ability Infernal Lore, even if you are normally not permitted to buy Arcane Abilities.",
+    flaw_type: "Story",
+    book: "Core",
+    major: true)
+
+  difficult_longevity_ritual = Flaw.create!(name: "Difficult Longevity Ritual"),
+    descrtiption: "Something in your magical nature makes it difficult to create an effective Longevity Ritual for you. Anyone (including yourself) cre- ating a Longevity Ritual for you must halve their Lab Total. You may create Longevity Rituals for others without penalty.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: true)
+
+  difficult_spontaneous_magic = Flaw.create!(name: "Difficult Spontaneous Magic"),
+    descrtiption: "Spontaneous magic is always an effort for you. You cannot cast spontaneous spells without exerting yourself. However, when you do exert yourself, you cast spells as any other magus.
+      This flaw may be combined with Weak Spontaneous Magic to create a magus who can- not use spontaneous magic at all.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: false)
+
+  disjointed_magic = Flaw.create!(name: "Disjointed Magic"),
+    descrtiption: "You cannot use previous knowledge to help you with magic. You gain no benefit from knowing a spell that is similar to one you are learning or inventing, and you gain no enchantment bonuses from Techniques and Forms already invested in an item.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: false)
+
+  difficult_underlings = Flaw.create!(name: "Difficlt Underlings"),
+    descrtiption: "You may only take this Story Hook if your character has, and will keep, underlings of some sort or another. No matter how many people you fire, or how carefully you vet new candidates, your underlings always cause prob- lems for you. The nature of the problems may change, but you are repeatedly drawn into dealing with the trouble they have caused. If other people give the same underlings orders, they have no problem. It’s not them, it’s you.",
+    flaw_type: "Story",
+    book: "Core",
+    major: true)
+
+  disfigured = Flaw.create!(name: "Disfigured"),
+    descrtiption: "A visible disfigurement makes you ugly and easy to recognize. Presence rolls that involve good looks and gaining respect from most people are at –3. You probably have a cruel nickname that refers to your unfortunate appearance. You might have a large, visible burn scar, or an enlarged and deformed eye (an “evil eye”), or even be albino.",
+    flaw_type: "General",
+    book: "Core",
+    major: false)
+
+  disorientating_magic = Flaw.create!(name: "Disorientating Magic"),
+    descrtiption: "After casting a spell, you must spend a round doing nothing but recovering your men- tal faculties.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: false)
+
+  driven_major = Flaw.create!(name: "Driven"),
+    descrtiption: "You have some goal which you are deter- mined to bring about. This has to be the sort of big goal that could shape an entire life, but if you do accomplish it you will immediately turn to a new project.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: true)
+
+  driven_minor = Flaw.create!(name: "Driven"),
+    descrtiption: "You have some goal which you are deter- mined to bring about. This has to be the sort of big goal that could shape an entire life, but if you do accomplish it you will immediately turn to a new project.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  dutybound = Flaw.create!(name: "Dutybound"),
+    descrtiption: "You adhere to a restrictive code of con- duct that forbids certain behavior, probably including prohibitions against lying, killing prisoners, stealing, and other occasionally use- ful actions. You follow this code out of guilt or fear rather than high-flown moral standards, and may spend more time justifying yourself than keeping your conduct pure.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  dwarf = Flaw.create!(name: "Dwarf"),
+    descrtiption: "You are the size of a child. Your comfort- able walking speed is two-thirds that of a nor- mal person. Your Size is –2, so the severity of wounds you take increases in three point incre- ments rather than five point increments (see page 171). You take a –1 penalty to each of Strength and Stamina, which may reduce each Characteristic as low as –6. You cannot take this Flaw and Giant Blood, Large, or Small Frame.",
+    flaw_type: "General",
+    book: "Core",
+    major: true)
+
+  enemies = Flaw.create!(name: "Enemies"),
+    descrtiption: "
+    Someone is causing trouble for you, such as a local baron or bishop, a band of outlaws, or a really nasty innkeeper. The enemy must be powerful enough to endanger you — this is best agreed on with the storyguide and the rest of the troupe.",
+    flaw_type: "Story",
+    book: "Core",
+    major: true)
+
+  enfeebled = Flaw.create!(name: "Enfeebled"),
+    descrtiption: "You cannot exert yourself for longer than a few seconds. Any need for rapid movement, such as combat or a chase, leaves you helpless. Long hikes are likewise beyond your capability. You are unable to learn Martial Abilities or any other skills involving physical exertion, since you cannot train in them. If you are a magus, you lose double the normal number of Fatigue levels from casting spells, but you may carry out laboratory activities as normal.",
+    flaw_type: "General",
+    book: "Core",
+    major: true)
+
+  envious_major = Flaw.create!(name: "Envious"),
+    descrtiption: "You believe that everyone has it better than you do, and that they don’t deserve their good fortune, while you do. Thus, you try to ruin their lives and take their goods for your- self. (This is not a good Flaw for a player char- acter, as it is likely to lead to conflict with the other players.)",
+    flaw_type: "Personality",
+    book: "Core",
+    major: true)
+
+  envious_minor = Flaw.create!(name: "Envious"),
+    descrtiption: "You believe that everyone has it better than you do, and that they don’t deserve their good fortune, while you do. Thus, you try to ruin their lives and take their goods for your- self. (This is not a good Flaw for a player char- acter, as it is likely to lead to conflict with the other players.)",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  faerie_friend = Flaw.create!(name: "Faerie Friend"),
+    descrtiption: "You have an ally among the fay. How much assistance you can get depends on the power of the friend — a small faerie with (relatively) minor powers can accompany you on a day to day basis. Powerful faeries have other business, and may only be available to answer questions and provide guidance, not to accompany you around on your adventures. Your ally is fully sentient, has its own powers, and can speak when it chooses. You may want to have another player act the part. While a faerie companion can be a great boon, it can also be a terrible burden. If it is nasty or mischievous, it could cause trouble for you everywhere. Characters with this Flaw can purchase the Arcane Knowledge Faerie Lore, even if they are normally restricted from purchasing it.",
+    flaw_type: "Story",
+    book: "Core",
+    major: false)
+
+  faerie_upbringing = Flaw.create!(name: "Faerie Upbringing"),
+    descrtiption: "Perhaps you were abandoned by your true kin, and the faeries found you. Maybe your fam- ily actually lived in a faerie forest, or faeries took you as a babe. Though you are now back in human society, you feel at home with and have an enhanced understanding of faeries, magic, and other strange things. However, you find human society, including religion, bizarre. You may learn Faerie Lore at character generation.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  favors = Flaw.create!(name: "Favors"),
+    descrtiption: "You owe a boon to someone (or to a great many people), and may be called upon to return the favor at any time. The consequences of ignoring such a request can range from mild to deadly serious, at the storyguide’s discretion.",
+    flaw_type: "Story",
+    book: "Core",
+    major: true)
+
+  fear = Flaw.create!(name: "Fear"),
+    descrtiption: "You are scared of something that you are likely to encounter fairly often. Its presence makes you edgy and very uncomfortable.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  feral_upbringing = Flaw.create!(name: "Feral Upbringing"),
+    descrtiption: "You grew up in the wilderness, either raised by wild animals or surviving on your own. For much of your life you could not speak, and knew nothing of human ways. Now that you have joined human society (or the covenant), you have learned to understand some basic spoken phrases, but civilized life is still a mystery you want little part of. You may only choose beginning Abilities that you could have learned in the wilds. In particular, you may not start with a score in a Language. In your first five years you gain 120 experience points, which must be split between (Area) Lore, Animal Handling, Athletics, Awareness, Brawl, Hunt, Stealth, Survival, and Swim.",
+    flaw_type: "General",
+    book: "Core",
+    major: false)
+
+  feud = Flaw.create!(name: "Feud"),
+    descrtiption: "Your family is involved in a feud. The opposing family or families are roughly of equivalent strength to your family and its allies. You are liable to be ambushed or attacked by opposing clansmen, and your family expects you to join raids against your enemies.",
+    flaw_type: "Story",
+    book: "Core",
+    major: true)
+
+  flawed_parma_magica = Flaw.create!(name: "Flawed Parma Magica"),
+    descrtiption: "Your Parma Magica is defective and pro- vides only half the normal Magic Resistance against a certain Form. You may purchase this Flaw more than once for different Forms.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: false)
+
+  fragile_constitution = Flaw.create!(name: "Fragile Constitution"),
+    descrtiption: "You are sickly and weak. You suffer a –3 penalty on all rolls to recover from wounds and diseases.",
+    flaw_type: "General",
+    book: "Core",
+    major: false)
+
+  fury = Flaw.create!(name: "Fury"),
+    descrtiption: "A violent temper sometimes overwhelms you, sending you into a destructive, uncontrol- lable rage. You are likely to be provoked by some sort of specific event, such as being insulted, being hurt, or hearing the Order of Hermes disparaged. Roll 9+ on a stress die to avoid flying into a rage when such an event occurs, with another roll every round to try to calm yourself should you fail the first. On a botch, you try to kill everyone around you. While enraged you get +3 to Damage, but –1 on all other scores and rolls.",
+    flaw_type: "Story",
+    book: "Core",
+    major: true)
+
+  generous_major = Flaw.create!(name: "Generous"),
+    descrtiption: "You willingly give away your property to anyone who expresses a desire for it, even if they are not particularly deserving. If some- thing is very important, you might not give it to someone, but you will certainly share it.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: true)
+
+  generous_minor = Flaw.create!(name: "Generous"),
+    descrtiption: "You willingly give away your property to anyone who expresses a desire for it, even if they are not particularly deserving. If some- thing is very important, you might not give it to someone, but you will certainly share it.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  greater_malediction = Flaw.create!(name: "Greater Malediction"),
+    descrtiption: "You have been cursed by some supernat- ural power, in a way that greatly hinders you. The effects of the curse should be comparable to those of other Major Flaws. Indeed, almost any greater Flaw could be the result of a curse.",
+    flaw_type: "Supernatural",
+    book: "Core",
+    major: true)
+
+  greedy_major = Flaw.create!(name: "Greedy"),
+    descrtiption: "You like to eat and drink, and do so to excess whenever given the chance. You are probably fat, but may not be if you rarely have the chance to indulge.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: true)
+
+  greedy_minor = Flaw.create!(name: "Greedy"),
+    descrtiption: "You like to eat and drink, and do so to excess whenever given the chance. You are probably fat, but may not be if you rarely have the chance to indulge.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  hatred_major = Flaw.create!(name: "Hatred"),
+    descrtiption: "You hate someone or some group so much that hurting them dominates your life. The tar- get of your hatred must not be someone you could easily overcome.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: true)
+
+  hatred_minor = Flaw.create!(name: "Hatred"),
+    descrtiption: "You hate someone or some group so much that hurting them dominates your life. The tar- get of your hatred must not be someone you could easily overcome.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  hedge_wizard = Flaw.create!(name: "Hedge Wizard"),
+    descrtiption: "Because of your esoteric magic, other magi distrust you, and more importantly, grant you no respect. You start with a negative Reputation within the Order of Hermes at level 3 as a hedge wizard, even though you are a member of the Order.",
+    flaw_type: "Hermetic",
+    book: "Core",
+    major: false)
+
+  heir = Flaw.create!(name: "Heir"),
+    descrtiption: "You have little power or wealth now, but you stand to inherit land and possibly money. You need do nothing special to remain in your position, but others may occasionally attempt to remove you from the line of succession — one way or another. When you finally win your inheritance you gain its responsibilities as well, so your freedom may be restricted.",
+    flaw_type: "Story",
+    book: "Core",
+    major: false)
+
+  higher_purpose = Flaw.create!(name: "Higher Purpose"),
+    descrtiption: "Let others concern themselves with petty matters, you have a nobler goal. This purpose should be altruistic, like freeing an oppressed people or bringing peace between the Order of Hermes and society.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
+  humble = Flaw.create!(name: "Humble"),
+    descrtiption: "You are humble, always willing to believe that others are more skilled and better than you. You do not necessarily underestimate your own abilities, you just think others are better.",
+    flaw_type: "Personality",
+    book: "Core",
+    major: false)
+
   # = Flaw.create!(name: ""),
   #   descrtiption: "",
   #   flaw_type: "",
