3e9a1226bbe2176fd2b0c96964fd5f1497710789
kevgarci <hello@kevingarcia.me>
2019-06-17 16:07:53 -0700

    refactor app.jsx as a default export

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+
+import { Route, Redirect, Switch } from "react-router-dom";
 // import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { ProtectedRoute } from '../util/route_util';
-import Modal from './modal/modal';
-import NavBarContainer from './nav_bar/nav_bar_container';
-import SideBarContainer from './side_bar/side_bar_container';
-import HomeContainer from './home/home_container';
-import CharacterShowContainer from './character/show_container';
+import { ProtectedRoute } from "../util/route_util";
+import Modal from "./modal/modal";
+import NavBarContainer from "./nav_bar/nav_bar_container";
+import SideBarContainer from "./side_bar/side_bar_container";
+import HomeContainer from "./home/home_container";
+import CharacterShowContainer from "./character/show_container";
 
+export default function App() {
+  return (
+    <div>
+      <Modal />
+      <Route path="/" component={NavBarContainer} />
+      {/* <ProtectedRoute path="/" component={ SideBarContainer } /> */}
+      <Switch>
+        <Route exact path="/" component={HomeContainer} />
+        {/* <Route path="/users/:userId" component={} /> */}
+        {/* <AuthRoute exact path="/" component={} /> */}
+        {/* <Route path="/users/:userId/characters" component={} /> */}
+        <Route
+          path="/characters/:characterId"
+          component={CharacterShowContainer}
+        />
 
-const App = () => (
-  <div>
-    <Modal />
-    <Route path="/" component={ NavBarContainer } />
-    {/* <ProtectedRoute path="/" component={ SideBarContainer } /> */}
-    <Switch>
-      <Route exact path="/" component={ HomeContainer } />
-      {/* <Route path="/users/:userId" component={} /> */}
-      {/* <AuthRoute exact path="/" component={} /> */}
-      {/* <Route path="/users/:userId/characters" component={} /> */}
-      <Route path="/characters/:characterId" component={ CharacterShowContainer } />
-
-      <Redirect from="*" to="/" />
-    </Switch>
-    <ProtectedRoute path="/" component={ SideBarContainer } />
-    {/* <Route exact path="/" component={ FooterContainer } /> */}
-  </div>
-);
-
-export default App;
+        <Redirect from="*" to="/" />
+      </Switch>
+      <ProtectedRoute path="/" component={SideBarContainer} />
+      {/* <Route exact path="/" component={ FooterContainer } /> */}
+    </div>
+  );
+}
